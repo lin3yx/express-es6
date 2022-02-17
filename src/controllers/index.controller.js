d77349a82df8521055fa695c8e94541f8e625c20
Yan Lin <lambertlin@outlook.com>
2022-02-16 22:25:48 -0500

    feature: improve demo endpoints

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,9 +1,43 @@
-const welcome = (req, res) => {
-  res.status(200).json({ msg: 'Welcome to the API' });
-};
+import _ from 'lodash';
+import fetch from 'node-fetch';
 
-const getAll = (req, res) => {
-  res.status(200).json({ msg: 'Get all' });
-};
+async function getPost(postId) {
+  const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const data = await post.json();
 
-export { welcome, getAll };
+  return data;
+}
+
+async function getAllPosts() {
+  const posts = [];
+
+  _.each(
+    Array.from({ length: 10 }, (v, i) => i + 1),
+    (id) => {
+      const post = getPost(id).then((response) => response);
+      posts.push(post);
+    },
+  );
+
+  try {
+    return await Promise.all(posts);
+  } catch (error) {
+    return error;
+  }
+}
+
+async function getPosts(req, res) {
+  const posts = await getAllPosts();
+
+  res.status(200).json({ posts });
+}
+
+async function getAddress(req, res) {
+  const address = await fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then((response) => response.json())
+    .then((user) => user.address);
+
+  res.status(200).json({ address });
+}
+
+export { getPosts, getAddress };
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { welcome, getAll } from '../controllers/index.controller';
+import { getPosts, getAddress } from '../controllers/index.controller';
 
 const path = '/test';
 const router = Router();
 
-router.get(path, welcome);
-router.get(`${path}/all`, getAll);
+router.get(path, getPosts);
+router.get(`${path}/address`, getAddress);
 
 export default router;
